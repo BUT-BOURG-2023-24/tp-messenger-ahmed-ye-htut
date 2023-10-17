c745c4c14f3a2d5f45c297d83915e06c7ac0a1d6
Ahmed Benmalek <ahmed.benmalek69@gmail.com>
2023-10-17 11:09:17 +0200

    add connection to database with .env adress

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,4 +1,6 @@
 import config from "../config";
+const mongoose = require('mongoose');
+require('dotenv/config');
 
 class Database 
 {
@@ -12,6 +14,26 @@ class Database
 	async connect()
 	{
 		// config.DB_ADDRESS contient l'adresse de la BDD
+		if(this.fromTest == true)
+		{
+			mongoose.connect(process.env.DB_ADRESS_TEST)
+
+			.then(() => {console.log("Connected to test DB !")})
+
+			.catch((error: Error) => 
+				{console.log("Error while connecting to test DB", error)
+			});
+		}
+		else
+		{
+			mongoose.connect(process.env.DB_ADRESS)
+
+			.then(() => {console.log("Connected to DB !")})
+
+			.catch((error: Error) => 
+				{console.log("Error while connecting to DB", error)
+			});
+		}
 	}
 }
 
