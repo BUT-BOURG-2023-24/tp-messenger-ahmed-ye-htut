e1a6fe72e06fc061dc6ffdc6eaab3f2992bccc94
Ye Htut Kyaw <yehtutkyawfr@gmail.com>
2023-10-17 11:07:00 +0200

    schema added

--- src/database/Mongo/Models/MessageModel.ts
+++ src/database/Mongo/Models/MessageModel.ts
@@ -1,14 +1,73 @@
 import mongoose, { Schema, Document } from "mongoose";
 import { MongooseID } from "../../../types";
 
+export enum Reactions {
+	HAPPY = "HAPPY",
+	SAD = "SAD",
+	THUMBSUP = "THUMBSUP",
+	THUMBSDOWN = "THUMBSDOWN",
+	LOVE = "LOVE",
+}
+
 export interface IMessage extends Document {
-	//A COMPLETER
+	conversationId: MongooseID;
+	from: MongooseID;
+	content: string;
+	postedAt: Date;
+	replyTo?: string | null;
+	edited: boolean;
+	deleted: boolean;
+	reactions: Map<MongooseID, Reactions>;
 }
 
 const MessageSchema: Schema<IMessage> = new Schema<IMessage>({
-	//A COMPLETER
+	conversationId:
+	{
+		type: Schema.Types.ObjectId,
+		ref: "Conversation",
+		required: true
+	},
+	from:
+	{
+		type: Schema.Types.ObjectId,
+		ref: "User",
+		required: true
+	},
+	content:
+	{
+		type: String,
+		required: true
+	},
+	postedAt:
+	{
+		type: Date,
+		default: Date.now,
+		required: true
+	},
+	replyTo:
+	{
+		type: Schema.Types.ObjectId,
+		ref: "Message",
+		default: null
+	},
+	edited:
+	{
+		type: Boolean,
+		default: false
+	},
+	deleted:
+	{
+		type: Boolean,
+		default: false
+	},
+	reactions:
+	{
+		type: Map,
+		of: String,
+		default: {}
+	},
 });
 
 const MessageModel = mongoose.model<IMessage>("Message", MessageSchema);
 
-export { MessageModel, MessageSchema };
+export default MessageModel;
